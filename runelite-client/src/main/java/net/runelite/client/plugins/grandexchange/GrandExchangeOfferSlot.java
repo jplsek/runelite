ae8962d29e9bc4ece55ea60519f3ae58f6a80cbe
Jeremy Plsek <jeremyplsek@gmail.com>
2019-06-01 19:16:14 +0200

    add ge link to offers

--- runelite-client/src/main/java/net/runelite/client/plugins/grandexchange/GrandExchangeOfferSlot.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/grandexchange/GrandExchangeOfferSlot.java
@@ -32,6 +32,7 @@ import java.awt.Color;
 import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.GridLayout;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
@@ -39,7 +40,9 @@ import java.awt.image.BufferedImage;
 import javax.annotation.Nullable;
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
+import javax.swing.JMenuItem;
 import javax.swing.JPanel;
+import javax.swing.JPopupMenu;
 import javax.swing.border.EmptyBorder;
 import net.runelite.api.GrandExchangeOffer;
 import net.runelite.api.GrandExchangeOfferState;
@@ -47,6 +50,7 @@ import static net.runelite.api.GrandExchangeOfferState.CANCELLED_BUY;
 import static net.runelite.api.GrandExchangeOfferState.CANCELLED_SELL;
 import static net.runelite.api.GrandExchangeOfferState.EMPTY;
 import net.runelite.api.ItemComposition;
+import static net.runelite.client.plugins.grandexchange.GrandExchangeItemPanel.geLink;
 import net.runelite.client.ui.ColorScheme;
 import net.runelite.client.ui.FontManager;
 import net.runelite.client.ui.components.ThinProgressBar;
@@ -73,6 +77,8 @@ public class GrandExchangeOfferSlot extends JPanel
 	private final JLabel offerSpent = new JLabel();
 
 	private final ThinProgressBar progressBar = new ThinProgressBar();
+
+	private final JMenuItem geLink = new JMenuItem("Open in Grand Exchange");
 
 	private boolean showingFace = true;
 
@@ -106,14 +112,12 @@ public class GrandExchangeOfferSlot extends JPanel
 			public void mouseEntered(MouseEvent mouseEvent)
 			{
 				super.mouseEntered(mouseEvent);
-				container.setBackground(ColorScheme.DARKER_GRAY_HOVER_COLOR);
 			}
 
 			@Override
 			public void mouseExited(MouseEvent mouseEvent)
 			{
 				super.mouseExited(mouseEvent);
-				container.setBackground(ColorScheme.DARKER_GRAY_COLOR);
 			}
 		};
 
@@ -123,7 +127,6 @@ public class GrandExchangeOfferSlot extends JPanel
 		JPanel faceCard = new JPanel();
 		faceCard.setBackground(ColorScheme.DARKER_GRAY_COLOR);
 		faceCard.setLayout(new BorderLayout());
-		faceCard.addMouseListener(ml);
 
 		itemIcon.setVerticalAlignment(JLabel.CENTER);
 		itemIcon.setHorizontalAlignment(JLabel.CENTER);
@@ -142,10 +145,15 @@ public class GrandExchangeOfferSlot extends JPanel
 		switchFaceViewIcon.setVerticalAlignment(JLabel.CENTER);
 		switchFaceViewIcon.setHorizontalAlignment(JLabel.CENTER);
 		switchFaceViewIcon.setPreferredSize(new Dimension(30, 45));
+		switchFaceViewIcon.addMouseListener(ml);
+
+		JPopupMenu menu = new JPopupMenu();
+		menu.add(geLink);
 
 		JPanel offerFaceDetails = new JPanel();
 		offerFaceDetails.setBackground(ColorScheme.DARKER_GRAY_COLOR);
 		offerFaceDetails.setLayout(new GridLayout(2, 1, 0, 2));
+		offerFaceDetails.setComponentPopupMenu(menu);
 
 		offerFaceDetails.add(itemName);
 		offerFaceDetails.add(offerInfo);
@@ -158,7 +166,7 @@ public class GrandExchangeOfferSlot extends JPanel
 		detailsCard.setBackground(ColorScheme.DARKER_GRAY_COLOR);
 		detailsCard.setLayout(new BorderLayout());
 		detailsCard.setBorder(new EmptyBorder(0, 15, 0, 0));
-		detailsCard.addMouseListener(ml);
+		detailsCard.setComponentPopupMenu(menu);
 
 		itemPrice.setForeground(Color.WHITE);
 		itemPrice.setVerticalAlignment(JLabel.BOTTOM);
@@ -173,6 +181,7 @@ public class GrandExchangeOfferSlot extends JPanel
 		switchDetailsViewIcon.setVerticalAlignment(JLabel.CENTER);
 		switchDetailsViewIcon.setHorizontalAlignment(JLabel.CENTER);
 		switchDetailsViewIcon.setPreferredSize(new Dimension(30, 45));
+		switchDetailsViewIcon.addMouseListener(ml);
 
 		JPanel offerDetails = new JPanel();
 		offerDetails.setBackground(ColorScheme.DARKER_GRAY_COLOR);
@@ -205,6 +214,12 @@ public class GrandExchangeOfferSlot extends JPanel
 
 			itemName.setText(offerItem.getName());
 			itemIcon.setIcon(new ImageIcon(itemImage));
+
+			for (ActionListener al : geLink.getActionListeners())
+			{
+				geLink.removeActionListener(al);
+			}
+			geLink.addActionListener(actionEvent -> geLink(offerItem.getName(), offerItem.getId()));
 
 			boolean buying = newOffer.getState() == GrandExchangeOfferState.BOUGHT
 				|| newOffer.getState() == GrandExchangeOfferState.BUYING
